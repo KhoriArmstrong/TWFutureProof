9573a2ee089509a3111ce1c291321870ce44493c
Khori Armstrong <khoriarmstrong@gmail.com>
2015-02-12 14:54:40 -0500

    The BASE project!

---
+++ app/src/androidTest/java/com/teamwagdin/owner/futureproof/TestLoginScreen.java
@@ -0,0 +1,11 @@
+package com.teamwagdin.owner.futureproof;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Admin on 2015-01-29.
+ */
+public class TestLoginScreen extends InstrumentationTestCase {
+    public TestLoginScreen() {
+    }
+}
